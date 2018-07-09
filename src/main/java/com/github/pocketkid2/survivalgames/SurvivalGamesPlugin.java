58bf6b28e62160abeb80f187e837cf9132221785
Adam <afranklint16@gmail.com>
2018-07-08 20:55:41 -0700

    Created plugin files and updated version

---
+++ src/main/java/com/github/pocketkid2/survivalgames/SurvivalGamesPlugin.java
@@ -0,0 +1,16 @@
+package com.github.pocketkid2.survivalgames;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SurvivalGamesPlugin extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		getLogger().info("Done!");
+	}
+
+	@Override
+	public void onDisable() {
+		getLogger().info("Done!");
+	}
+}
