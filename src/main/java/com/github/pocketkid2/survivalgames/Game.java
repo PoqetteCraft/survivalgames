ab17199f4166a863b4e611cdf88f90a533228a0b
Adam <afranklint16@gmail.com>
2018-07-13 14:40:11 -0700

    Add enum and initialization to game!

--- src/main/java/com/github/pocketkid2/survivalgames/Game.java
+++ src/main/java/com/github/pocketkid2/survivalgames/Game.java
@@ -1,22 +1,46 @@
 package com.github.pocketkid2.survivalgames;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.bukkit.ChatColor;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 
 // Represents an automated game
 public class Game {
 
+	public enum Status {
+
+		RESETTING(ChatColor.AQUA + "Resetting map"),
+		WAITING(ChatColor.GREEN + "Waiting for players"),
+		STARTING(ChatColor.GOLD + "Counting down"),
+		IN_GAME(ChatColor.RED + "In-game");
+
+		private String readable;
+
+		public String getReadable() {
+			return readable;
+		}
+
+		Status(String s) {
+			readable = s;
+		}
+	}
+
 	// Each game has a map
 	private Map map;
-
-	// Each game has a name
-	private String name;
 
 	// Each game has a list of alive/active players
 	private List<Player> activePlayers;
 
 	// Each game has a list of dead/offline players
 	private List<OfflinePlayer> inactivePlayers;
+
+	// Initialization constructor
+	public Game(Map m) {
+		map = m;
+		activePlayers = new ArrayList<Player>();
+		inactivePlayers = new ArrayList<OfflinePlayer>();
+	}
 }
